8122290f3777c3db49f6ccd9facedb326088c8e1
Csaba Kiraly <csaba.kiraly@gmail.com>
2023-03-22 11:31:57 +0100

    add SparseBlock with selected rows and columns only
    SparseBlock stores only selected rows and columns.
    Initial implementation without checks and asserts.
    
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>

--- DAS/block.py
+++ DAS/block.py
@@ -81,3 +81,79 @@ class Block:
             print(line+"|")
         print(dash)
 
+class SparseBlock:
+
+    def __init__(self, size, rows, columns):
+        self.blockSize = size
+        self.rows = {r: zeros(self.blockSize) for r in rows}
+        self.columns = {r: zeros(self.blockSize) for r in columns}
+
+    def fill(self):
+        for line in self.rows:
+            line.setall(1)
+        for line in self.columns:
+            line.setall(1)
+
+    def merge(self, merged):
+        for id in self.rows:
+            self.mergeRow(id, merged.getRow(id))
+        for id in self.columns:
+            self.mergeColumn(id, merged.getColumn(id))
+
+    def getSegment(self, rowID, columnID):
+        """Check whether a segment is included"""
+        ret = 0
+
+        r = self.rows.get(rowID)
+        if r:
+            ret |= r[columnID]
+
+        c = self.columns.get(columnID)
+        if c:
+            ret |= c[rowID]
+
+        return ret
+
+    def setSegment(self, rowID, columnID, value = 1):
+        """Set value for a segment (default 1)"""
+        r = self.rows.get(rowID)
+        if r:
+            r[columnID] = 1
+
+        c = self.columns.get(columnID)
+        if c:
+            c[rowID] = 1
+
+    def getColumn(self, id):
+        return self.columns[id]
+
+    def mergeColumn(self, id, column):
+        self.columns[id] |= column
+        for xid, line in self.rows.items():
+            line[id] |= self.columns[id][xid]
+
+    def repairColumn(self, id):
+        success = self.columns[id].count(1)
+        if success >= self.blockSize/2:
+            ret = ~self.columns[id]
+            self.columns[id].setall(1)
+        else:
+            ret = zeros(self.blockSize)
+        return ret
+
+    def getRow(self, id):
+        return self.rows[id]
+
+    def mergeRow(self, id, row):
+        self.rows[id] |= row
+        for xid, line in self.columns.items():
+            line[id] |= self.rows[id][xid]
+
+    def repairRow(self, id):
+        success = self.rows[id].count(1)
+        if success >= self.blockSize/2:
+            ret = ~self.rows[id]
+            self.rows[id].setall(1)
+        else:
+            ret = zeros(self.blockSize)
+        return ret
